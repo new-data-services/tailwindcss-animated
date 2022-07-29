8c6242fba63b1ddf0e463c59edccc181aa04b023
Nils Meyer zu Borgsen <meyerzuborgsen@new-data-services.de>
2022-07-29 17:06:26 +0200

    added option functions and url parameters

--- configurator/js/app.js
+++ configurator/js/app.js
@@ -5,11 +5,85 @@ Alpine.store('properties', {
 
     // Set default values here
 
-    timing: "animate-once",
-    direction: "animate-forward",
-    fillmode: "animate-fill-none",
+    timing: "once",
+    direction: null,
+    fillmode: null,
     duration: null,
     delay: null,
+    easing: null,
+    currentAnimation: "wiggle",
+
+    modifyProperty(property, newValue) {
+        if (property == "timing") {
+            this.timing = newValue;
+        }
+        else if (property == "direction") {
+            this.direction = newValue;
+        }
+        else if (property == "fillmode") {
+            this.fillmode = newValue;
+        }
+        else if (property == "duration") {
+            this.duration = newValue;
+        }
+        else if (property == "delay") {
+            this.delay = newValue;
+        }
+        else if (property == "easing") {
+            this.easing = newValue;
+        }
+        else if (property == "currentAnimation") {
+            this.currentAnimation = newValue;
+        }
+        
+        this.update()
+    },
+
+    update() {
+        //reload preview objects
+        this.setParams();
+        return
+    },
+
+    setParams() {
+        //set params to URL
+        var url = new URL(window.location.href);
+        url.searchParams.set('t', this.timing);
+        url.searchParams.set('di', this.direction);
+        url.searchParams.set('f', this.fillmode);
+        url.searchParams.set('du', this.duration);
+        url.searchParams.set('de', this.delay);
+        url.searchParams.set('e', this.easing);
+        url.searchParams.set('a', this.currentAnimation);
+        window.history.pushState(window.location.href, '', url);
+    },
+
+    init() {
+        //pull params from URL
+        const queryString = window.location.search;
+        const urlParams = new URLSearchParams(queryString);
+        if (urlParams.get('t') != null && urlParams.get('t') != '') {
+            this.timing = urlParams.get('t');
+        }
+        if (urlParams.get('di') != null && urlParams.get('di') != '') {
+            this.direction = urlParams.get('di');
+        }
+        if (urlParams.get('f') != null && urlParams.get('f') != '') {
+            this.fillmode = urlParams.get('f');
+        }
+        if (urlParams.get('du') != null && urlParams.get('du') != '') {
+            this.duration = urlParams.get('du');
+        }
+        if (urlParams.get('de') != null && urlParams.get('de') != '') {
+            this.delay = urlParams.get('de');
+        }
+        if (urlParams.get('e') != null && urlParams.get('e') != '') {
+            this.easing = urlParams.get('e');
+        }
+        if (urlParams.get('a') != null && urlParams.get('a') != '') {
+        this.currentAnimation = urlParams.get('a');
+        }
+    }
 })
 
 Alpine.plugin(collapse);
