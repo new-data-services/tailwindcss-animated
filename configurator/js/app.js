9da17a9a25ece721e7c5fbcc62959941a87b3c6b
Nils Meyer zu Borgsen <meyerzuborgsen@new-data-services.de>
2022-07-29 12:58:08 +0200

    add store with default properties and print them initially

--- configurator/js/app.js
+++ configurator/js/app.js
@@ -1,5 +1,17 @@
 import Alpine from 'alpinejs';
 import collapse from '@alpinejs/collapse';
 
+Alpine.store('properties', {
+
+    // Set default values here
+
+    timing: "animate-once",
+    direction: "animate-forward",
+    fillmode: "animate-fill-none",
+    duration: null,
+    delay: null,
+})
+
 Alpine.plugin(collapse);
 Alpine.start();
+
