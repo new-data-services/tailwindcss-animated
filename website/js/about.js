1ea95da92788b1276f06a9c170430bcac5e46690
Lars Heidkämper <heidkaemper@new-data-services.de>
2022-08-18 13:50:51 +0200

    remove about.js

--- website/js/about.js
+++
@@ -1,7 +0,0 @@
-import Alpine from 'alpinejs';
-import collapse from '@alpinejs/collapse';
-import intersect from '@alpinejs/intersect';
-
-Alpine.plugin(collapse);
-Alpine.plugin(intersect);
-Alpine.start();
