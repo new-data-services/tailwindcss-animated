24cead9c069dba77ddfb6faf1f0a43e1ef32a248
Lars Heidkämper <heidkaemper@new-data-services.de>
2022-08-22 14:46:56 +0200

    config cleanup

--- src/theme.js
+++ src/theme.js
@@ -2,87 +2,169 @@ module.exports = {
     extend: {
         keyframes: {
             'wiggle': {
-                '0%, 100%': { transform: 'rotate(-3deg)' },
-                '50%': { transform: 'rotate(3deg)' },
+                '0%, 100%': {
+                    transform: 'rotate(-3deg)',
+                },
+                '50%': {
+                    transform: 'rotate(3deg)',
+                },
             },
             'wiggle-more': {
-                '0%, 100%': { transform: 'rotate(-12deg)' },
-                '50%': { transform: 'rotate(12deg)' },
-            },
-            'fade-down': {
-                '0%': { opacity: '0', transform: 'translateY(-2rem)' },
-                '100%': { opacity: '1', transform: 'translateY(0)' },
-            },
-            'fade-up': {
-                '0%': { opacity: '0', transform: 'translateY(2rem)' },
-                '100%': { opacity: '1', transform: 'translateY(0)' },
-            },
-            'fade-left': {
-                '0%': { opacity: '0', transform: 'translateX(2rem)' },
-                '100%': { opacity: '1', transform: 'translateX(0)' },
-            },
-            'fade-right': {
-                '0%': { opacity: '0', transform: 'translateX(-2rem)' },
-                '100%': { opacity: '1', transform: 'translateX(0)' },
+                '0%, 100%': {
+                    transform: 'rotate(-12deg)',
+                },
+                '50%': {
+                    transform: 'rotate(12deg)',
+                },
             },
             'rotate-y': {
-                '0%': { transform: 'rotateY(360deg)' },
-                '100%': { transform: 'rotateY(0)' },
+                '0%': {
+                    transform: 'rotateY(360deg)',
+                },
+                '100%': {
+                    transform: 'rotateY(0)',
+                },
             },
             'rotate-x': {
-                '0%': { transform: 'rotateX(360deg)' },
-                '100%': { transform: 'rotateX(0)' },
-            },
-            'flip-up': {
-                '0%': { transform: 'rotateX(90deg)', transformOrigin: 'bottom' },
-                '100%': { transform: 'rotateX(0)' },
-            },
-            'flip-down': {
-                '0%': { transform: 'rotateX(-90deg)', transformOrigin: 'top' },
-                '100%': { transform: 'rotateX(0)' },
+                '0%': {
+                    transform: 'rotateX(360deg)',
+                },
+                '100%': {
+                    transform: 'rotateX(0)',
+                },
             },
             'jump': {
-                '0%,100%': { transform: 'scale(100%)' },
-                '10%': { transform: 'scale(80%)' },
-                '50%': { transform: 'scale(120%)' },
+                '0%, 100%': {
+                    transform: 'scale(100%)',
+                },
+                '10%': {
+                    transform: 'scale(80%)',
+                },
+                '50%': {
+                    transform: 'scale(120%)',
+                },
             },
             'jump-in': {
-                '0%': { transform: 'scale(0%) '},
-                '80%': { transform: 'scale(120%)' },
-                '100%': { transform: 'scale(100%)' },
+                '0%': {
+                    transform: 'scale(0%)',
+                },
+                '80%': {
+                    transform: 'scale(120%)',
+                },
+                '100%': {
+                    transform: 'scale(100%)',
+                },
             },
             'jump-out': {
-                '0%': { transform: 'scale(100%)' },
-                '20%': { transform: 'scale(120%)' },
-                '100%': { transform: 'scale(0%)' },
+                '0%': {
+                    transform: 'scale(100%)',
+                },
+                '20%': {
+                    transform: 'scale(120%)',
+                },
+                '100%': {
+                    transform: 'scale(0%)',
+                },
             },
             'shake': {
-                '0%': { transform: 'translateX(0rem)' },
-                '25%': { transform: 'translateX(-1rem)' },
-                '75%': { transform: 'translateX(1rem)' },
-                '100%': { transform: 'translateX(0rem)' },
+                '0%': {
+                    transform: 'translateX(0rem)',
+                },
+                '25%': {
+                    transform: 'translateX(-1rem)',
+                },
+                '75%': {
+                    transform: 'translateX(1rem)',
+                },
+                '100%': {
+                    transform: 'translateX(0rem)',
+                },
             },
             'fade': {
-                '0%': { opacity: '0' },
-                '100%': { opacity: '1' },
+                '0%': {
+                    opacity: '0',
+                },
+                '100%': {
+                    opacity: '1',
+                },
+            },
+            'fade-down': {
+                '0%': {
+                    opacity: '0',
+                    transform: 'translateY(-2rem)',
+                },
+                '100%': {
+                    opacity: '1',
+                    transform: 'translateY(0)',
+                },
+            },
+            'fade-up': {
+                '0%': {
+                    opacity: '0',
+                    transform: 'translateY(2rem)',
+                },
+                '100%': {
+                    opacity: '1',
+                    transform: 'translateY(0)',
+                },
+            },
+            'fade-left': {
+                '0%': {
+                    opacity: '0',
+                    transform: 'translateX(2rem)',
+                },
+                '100%': {
+                    opacity: '1',
+                    transform: 'translateX(0)',
+                },
+            },
+            'fade-right': {
+                '0%': {
+                    opacity: '0',
+                    transform: 'translateX(-2rem)',
+                },
+                '100%': {
+                    opacity: '1',
+                    transform: 'translateX(0)',
+                },
+            },
+            'flip-up': {
+                '0%': {
+                    transform: 'rotateX(90deg)',
+                    transformOrigin: 'bottom',
+                },
+                '100%': {
+                    transform: 'rotateX(0)',
+                    transformOrigin: 'bottom',
+                },
+            },
+            'flip-down': {
+                '0%': {
+                    transform: 'rotateX(-90deg)',
+                    transformOrigin: 'top',
+                },
+                '100%': {
+                    transform: 'rotateX(0)',
+                    transformOrigin: 'top',
+                },
             },
         },
         animation: {
-            'wiggle': 'wiggle 1s infinite',
-            'wiggle-more': 'wiggle-more 1s infinite',
-            'fade-down': 'fade-down 1s both',
-            'fade-up': 'fade-up 1s both',
-            'fade-left': 'fade-left 1s both',
-            'fade-right': 'fade-right 1s both',
+            'wiggle': 'wiggle 1s',
+            'wiggle-more': 'wiggle-more 1s',
             'rotate-y': 'rotate-y 1s',
             'rotate-x': 'rotate-x 1s',
-            'flip-up': 'flip-up 1s',
-            'flip-down': 'flip-down 1s',
             'jump': 'jump .5s',
             'jump-in': 'jump-in .5s',
             'jump-out': 'jump-out .5s',
             'shake': 'shake .5s',
-            'fade': 'fade .5s',
+            'fade': 'fade 1s',
+            'fade-down': 'fade-down 1s',
+            'fade-up': 'fade-up 1s',
+            'fade-left': 'fade-left 1s',
+            'fade-right': 'fade-right 1s',
+            'flip-up': 'flip-up 1s',
+            'flip-down': 'flip-down 1s',
         },
         animationDelay: {
             none: '0ms',
