d1cce4cae214d8d6bb06d4f52a1a1a9629d4f112
Lars Heidkämper <heidkaemper@new-data-services.de>
2025-01-28 08:53:38 +0100

    update tests

--- jest/legacy.test.js
+++ jest/legacy.test.js
@@ -1,15 +1,14 @@
-const fs = require('fs')
 const path = require('path')
 const postcss = require('postcss')
-const tailwindcss = require('tailwindcss')
+const tailwindcss = require('tailwindcss-legacy')
 
 async function run(file) {
     const { currentTestName } = expect.getState()
 
     const config = {
-        content: [file],
+        content: [`./jest/${file}`],
         corePlugins: { preflight: false },
-        plugins: [require('../../src')],
+        plugins: [require('../src')],
     }
 
     const result = await postcss(tailwindcss(config)).process('@tailwind utilities', {
@@ -20,12 +19,8 @@ async function run(file) {
 }
 
 describe('legacy tailwind', () => {
-    if (! fs.existsSync(`${__dirname}/node_modules`)) {
-        throw new Error(`Dependencies in ${__dirname} are missing`)
-    }
-
     it('generates `composition` utilities', async () => {
-        expect(await run('jest/content/composition.html')).toIncludeAll([
+        expect(await run('content/composition.html')).toIncludeAll([
             '.animate-add { animation-composition: add; }',
             '.animate-replace { animation-composition: replace; }',
             '.animate-accumulate { animation-composition: accumulate; }',
@@ -33,35 +28,35 @@ describe('legacy tailwind', () => {
     })
 
     it('should add `delay` utilities', async () => {
-        expect(await run('jest/content/delay.html')).toIncludeAll([
+        expect(await run('content/delay.html')).toIncludeAll([
             '.animate-delay-75 { animation-delay: 75ms; }',
             '.animate-delay-\\[666ms\\] { animation-delay: 666ms; }',
         ])
     })
 
     it('generates `direction` utilities', async () => {
-        expect(await run('jest/content/direction.html')).toIncludeAll([
+        expect(await run('content/direction.html')).toIncludeAll([
             '.animate-normal { animation-direction: normal; }',
             '.animate-reverse { animation-direction: reverse; }',
         ])
     })
 
     it('generates `duration` utilities', async () => {
-        expect(await run('jest/content/duration.html')).toIncludeAll([
+        expect(await run('content/duration.html')).toIncludeAll([
             '.animate-duration-75 { animation-duration: 75ms; }',
             '.animate-duration-\\[666ms\\] { animation-duration: 666ms; }',
         ])
     })
 
     it('generates `fill-mode` utilities', async () => {
-        expect(await run('jest/content/fill-mode.html')).toIncludeAll([
+        expect(await run('content/fill-mode.html')).toIncludeAll([
             '.animate-fill-both { animation-fill-mode: both; }',
             '.animate-fill-none { animation-fill-mode: normal; }',
         ])
     })
 
     it('generates `iteration-count` utilities', async () => {
-        expect(await run('jest/content/iteration-count.html')).toIncludeAll([
+        expect(await run('content/iteration-count.html')).toIncludeAll([
             '.animate-infinite { animation-iteration-count: infinite; }',
             '.animate-once { animation-iteration-count: 1; }',
             '.animate-iteration-\\[14\\] { animation-iteration-count: 14; }',
@@ -69,14 +64,14 @@ describe('legacy tailwind', () => {
     })
 
     it('generates `play-state` utilities', async () => {
-        expect(await run('jest/content/play-state.html')).toIncludeAll([
+        expect(await run('content/play-state.html')).toIncludeAll([
             '.animate-play { animation-play-state: running; }',
             '.animate-stop { animation-play-state: paused; }',
         ])
     })
 
     it('generates predefined animations', async () => {
-        expect(await run('jest/content/predefined-animations.html')).toIncludeAll([
+        expect(await run('content/predefined-animations.html')).toIncludeAll([
             '.animate-fade { animation: fade var(--default-animation-duration, 1s) var(--default-animation-timing-function, ease) var(--default-animation-delay, 0s) both; }',
             '@keyframes fade { 0% { opacity: 0; } 100% { opacity: 1; }}',
             '.animate-spin { animation: spin var(--default-animation-duration, 1s) var(--default-animation-timing-function, linear) var(--default-animation-delay, 0s) infinite; }',
@@ -85,7 +80,7 @@ describe('legacy tailwind', () => {
     })
 
     it('generates `timing-function` utilities', async () => {
-        expect(await run('jest/content/timing-function.html')).toIncludeAll([
+        expect(await run('content/timing-function.html')).toIncludeAll([
             '.animate-ease { animation-timing-function: ease; }',
             '.animate-ease-linear { animation-timing-function: linear; }',
             '.animate-ease-\\[cubic-bezier\\(1\\2c 0\\.66\\2c 0\\.33\\2c 0\\)\\] { animation-timing-function: cubic-bezier(1,0.66,0.33,0); }',
